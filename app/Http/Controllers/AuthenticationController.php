f953bed4b0a754ded29df81262f8531cf6f8f1a8
SebastianLontis <161430885+SebastianLontis@users.noreply.github.com>
2024-07-08 22:37:13 +0300

    authentification works(login,logout,register)

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class AuthenticationController extends Controller
 {
-    //
+    public function login(Request $request): JsonResponse
+    {
+
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->get()->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        // Revoke the token that was used to authenticate the current request
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+    
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => $user
+        ], 201);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Login
+Route::post('/login', [AuthenticationController::class, 'login'])->withoutMiddleware('auth:sanctum');
+
+// Logout
+Route::post('logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+
+// Register
+Route::post('/register', [AuthenticationController::class, 'register'])->withoutMiddleware('auth:sanctum');
